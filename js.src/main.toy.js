78254a0cf22b7ea10694d0e9f5e5cc923688a93f
ten <ten@mutate.space>
2017-05-06 23:25:00 +0200

    source folders

---
+++ js.src/main.toy.js
@@ -0,0 +1,13 @@
+require('jquery')
+require('three')
+
+require('./three.js')
+require('./watch.js')
+require('./urndr.js')
+require('./toy.js')
+require('./modules_toy.js')
+
+require('./GIFEncoder/LZWEncoder.min.js')
+require('./GIFEncoder/NeuQuant.min.js')
+require('./GIFEncoder/GIFEncoder.min.js')
+require('./GIFEncoder/b64.js')
