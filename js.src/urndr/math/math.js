254dd5e5df9ff78b79bd21d20b81d661304d6a01
ten <ten@mutate.space>
2017-05-13 15:10:56 +0200

    urndr uuid function

--- js.src/urndr/math/math.js
+++ js.src/urndr/math/math.js
@@ -1,5 +1,17 @@
 export default {
 
+    uuid: () => {
+
+        const s4 = () => {
+
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+
+        }
+
+        return s4() + '-' + s4() + '-' + s4() + '-' + s4()
+
+    },
+
     pixelToCoordinate: function( x , y , w , h ) {
         return {
             x : THREE.Math.mapLinear( x , 0 , w , -1 , 1 ),
